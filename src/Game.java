b2827d0be798b057dde9bda7c44b5e99afabef0a
Finn Busse <finnbusse@outlook.de>
2025-01-13 08:50:03 +0100

    add crash logic

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import sas.*;
+
 import java.awt.Color;
 
 public class Game {
@@ -7,6 +8,7 @@ public class Game {
     Auto auto;
     Obstacle[] hindernisse;
 
+    boolean gameOver;
 
 
     public static void main(String[] args) {
@@ -15,13 +17,13 @@ public class Game {
 
     public Game() {
 
-        window = new View(1000,700);
-        auto = new Auto(50,300);
+        window = new View(1000, 700);
+        auto = new Auto(50, 300);
 
         hindernisse = new Obstacle[6];
 
-        for (int i =0; i<6; i++) {
-            hindernisse[i] = new Obstacle(100+i*400, i*150);
+        for (int i = 0; i < 6; i++) {
+            hindernisse[i] = new Obstacle(100 + i * 500, i * 200);
         }
 
         startGame();
@@ -29,7 +31,7 @@ public class Game {
     }
 
     public void startGame() {
-        while (true) {
+        while (gameOver == !true) {
             window.wait(1);
 
             if (window.keyUpPressed()) {
@@ -43,12 +45,20 @@ public class Game {
 
             }
 
-            for (int i = 0; i<6; i++) {
-                hindernisse[i].move(-1,0);
+            for (int i = 0; i < 6; i++) {
+                hindernisse[i].move(-0.5, 0);
 
-                if (hindernisse[i].getX < 0) {
+                if (hindernisse[i].getX() < -100) {
                     hindernisse[i].setX(1000);
                 }
+
+                if (auto.getX() < hindernisse[i].getX() + 100 &&
+                        auto.getX() + 50 > hindernisse[i].getX() &&
+                        auto.getY() < hindernisse[i].getY() + 100 &&
+                        auto.getY() + 100 > hindernisse[i].getY()) {
+                    gameOver = true;
+                }
+
             }
 
 
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,41 +1,51 @@
 import sas.*;
 import java.awt.Color;
 
+
 public class Obstacle {
 
-    Rectangle block;
-    private double x, y, pX, pY;
+
+    private Rectangle block;
+
+
+    private double x;
+
+
+    private double y;
+
 
     public Obstacle(double pX, double pY) {
-        this.x = pX;
-        this.y = pY;
-        this.block = new Rectangle(pX,pY,100,100,Color.RED);
-
+        x = pX;
+        y = pY;
+        block = new Rectangle(pX, pY, 100, 100, Color.RED);
     }
 
-    public void move(double pX, double pY) {
-        this.x = pX;
-        this.y = pY;
 
-
-        //this.block.moveTo(this.pX + this.getX(), this.pY + this.getY());
-        this.block.move(pX, pY);
-
-
-
-
+    public void move(double deltaX, double deltaY) {
+        x += deltaX;
+        y += deltaY;
+        block.move(deltaX, deltaY);
     }
+
 
     public void setX(double pX) {
-        this.x = pX;
-        this.block.moveTo(this.x, this.y);
+        x = pX;
+        block.moveTo(x, y);
     }
+
+
+    public void setY(double pY) {
+        y = pY;
+        block.moveTo(x, y);
+    }
+
 
     public double getX() {
-        return this.x;
+        return x;
     }
 
+
     public double getY() {
-        return this.y;
+        return y;
     }
 }
